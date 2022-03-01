3b909632143fac273261872d83bb74da03beeb1f
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-02-28 21:50:06 -0500

    Create multiple_assertions.js

---
+++ cypress/integration/multiple_assertions.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+
+    cy
+      .visit('/board/42257192965')
+  })
+  
+  it('Multiple assertions', () => {
+  
+    cy
+      .get('[data-cy=task]')   //then does not contain retry logic - no 4 second "wait" period - would need to be a .should instead
+      .then(item => {
+        if (item.length !== 3) {
+          throw new Error('Not enough elements!')
+        }                              
+        expect(item[0]).to.contain.text('tasks')
+        expect(item[1]).to.contain.text('Milk')
+      })
+  
+  })
