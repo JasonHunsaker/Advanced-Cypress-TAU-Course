155856ae8ff92818de15af2c8ed288a467e25361
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-03-04 22:21:57 -0500

    Update Stubbing_Responses.js

--- cypress/integration/Stubbing_Responses.js
+++ cypress/integration/Stubbing_Responses.js
@@ -2,34 +2,40 @@
 
 it('Stubbing response', () => {
 
-    // cy
-    //   .intercept({
-    //     method: 'GET',
-    //     url: '/api/boards'
-    //   }, {
-    //       fixture: 'threeBoards' //this replaces the array on the server with what we put here - it could be empty 'Body:[]', or we can use our own values
-    //   }).as('boardList')  
+    cy
+      .intercept({
+        method: 'GET',
+        url: '/api/boards'       //we can also put a function here
+      }, (req) => {  
+            
+        req.reply( (res) => {
+           res.body[0].starred = true
+
+           return res
+        })
+          
+      }).as('boardList')  
   
 
 
-    cy
-    .intercept({
-            method: 'POST',
-            url: '/api/boards'
-          }, {
-              forceNetworkError: true //we can also do other testing of issues with cypress, like having a network error
-          }).as('boardList')
+    // cy
+    // .intercept({
+    //         method: 'POST',
+    //         url: '/api/boards'
+    //       }, {
+    //           forceNetworkError: true //we can also do other testing of issues with cypress, like having a network error
+    //       }).as('boardList')
     cy
       .visit('localhost:3000')
 
-    cy  
-        .get('[data-cy=create-board]')
-        .click()
+    // cy  
+    //     .get('[data-cy=create-board]')
+    //     .click()
     
-        .get('[data-cy=new-board-input]')
-        .type('new board{enter}')
+    //     .get('[data-cy=new-board-input]')
+    //     .type('new board{enter}')
 
-        .get('#errorMessage')
-        .should('be.visible')
+    //     .get('#errorMessage')
+    //     .should('be.visible')
           
   });
