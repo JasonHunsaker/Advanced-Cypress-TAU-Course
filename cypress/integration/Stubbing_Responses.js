365b1b52d376480d40e9e1a28ad7b5f6a04f10e1
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-03-04 22:15:21 -0500

    Update Stubbing_Responses.js

--- cypress/integration/Stubbing_Responses.js
+++ cypress/integration/Stubbing_Responses.js
@@ -2,15 +2,34 @@
 
 it('Stubbing response', () => {
 
-    cy
-      .intercept({
-        method: 'GET',
-        url: '/api/boards'
-      }, {
-          body: []
-      }).as('boardList')
+    // cy
+    //   .intercept({
+    //     method: 'GET',
+    //     url: '/api/boards'
+    //   }, {
+    //       fixture: 'threeBoards' //this replaces the array on the server with what we put here - it could be empty 'Body:[]', or we can use our own values
+    //   }).as('boardList')  
   
+
+
+    cy
+    .intercept({
+            method: 'POST',
+            url: '/api/boards'
+          }, {
+              forceNetworkError: true //we can also do other testing of issues with cypress, like having a network error
+          }).as('boardList')
     cy
       .visit('localhost:3000')
-  
+
+    cy  
+        .get('[data-cy=create-board]')
+        .click()
+    
+        .get('[data-cy=new-board-input]')
+        .type('new board{enter}')
+
+        .get('#errorMessage')
+        .should('be.visible')
+          
   });
