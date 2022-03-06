57d6c31918c956800c9118e44746edc4fdee7baf
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-03-05 21:38:50 -0500

    Create Commands.d.ts

---
+++ cypress/support/Commands.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+    interface Chainable{
+
+        /**Creates a new board via UI
+         * just put the board name ('here')
+         */
+        addBoard()
+    }
+
+     
+}
+
